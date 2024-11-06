b103bae256e13e03158fb3b5bd38a611033ffd81
Robin <robin.hasler@bluewin.ch>
2024-11-06 15:37:22 +0100

    vite config anpassungen

--- src/components/ShopItems.jsx
+++ src/components/ShopItems.jsx
@@ -70,7 +70,7 @@ const products = [
 
 export default function Example() {
   return (
-    <div className="bg-white">
+    <div>
       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
         <h2 className="sr-only">Produkte</h2>
 
